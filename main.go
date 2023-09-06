6a975c8e3127bedb256c1aa78261cd83bddd779e
Miran Hamid <miranhamid2002@gmail.com>
2023-09-06 21:21:11 +0530

    add CRUD fucntions

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
-	"github.com/thedevaddams/renderer"
+	"github.com/thedevsaddam/renderer"
 	mgo "gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
@@ -16,21 +18,21 @@ var rnd *renderer.Render
 var db *mgo.Database
 
 const (
-	hostName       string = "localhost:27017"
+	hostName       string = "127.0.0.1:27017"
 	dbName         string = "demo_todo"
 	collectionName string = "todo"
-	port           string = ":8081"
+	port           string = ":9000"
 )
 
 type (
-	todoModel struct {
+	itemModel struct {
 		ID        bson.ObjectId `bson:"_id,omitempty"`
 		Title     string        `bson:"title"`
 		Completed bool          `bson:"completed"`
 		CreatedAt time.Time     `bson:"createdAt"`
 	}
 
-	todo struct {
+	item struct {
 		ID        string    `json:"id"`
 		Title     string    `json:"title"`
 		Completed bool      `json:"completed"`
@@ -47,13 +49,137 @@ func init() {
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	err := rnd.Template(w, http.StatusOK, []string{"static/home.tpl"}, nil)
+	err := rnd.Template(w, http.StatusOK, []string{"static/home.html"}, nil)
 	checkErr(err)
 }
 
 func fetchItems(w http.ResponseWriter, r *http.Request) {
-todos:
-	[]todoModel{}
+	items := []itemModel{}
+
+	if err := db.C(collectionName).Find(bson.M{}).All(&items); err != nil {
+		rnd.JSON(w, http.StatusProcessing, renderer.M{
+			"message": "Failed to load items",
+			"error":   err,
+		})
+		return
+	}
+
+	itemList := []item{}
+
+	for _, t := range items {
+		itemList = append(itemList, item{
+			ID:        t.ID.Hex(),
+			Title:     t.Title,
+			Completed: t.Completed,
+			CreatedAt: t.CreatedAt,
+		})
+	}
+	rnd.JSON(w, http.StatusOK, renderer.M{
+		"data": itemList,
+	})
+}
+
+func createItem(w http.ResponseWriter, r *http.Request) {
+	var t item
+
+	if err := json.NewDecoder(r.Body).Decode(&t); err != nil {
+		rnd.JSON(w, http.StatusProcessing, err)
+		return
+	}
+
+	if t.Title == "" {
+		rnd.JSON(w, http.StatusBadRequest, renderer.M{
+			"message": "The Title is required",
+		})
+		return
+	}
+
+	im := itemModel{
+		ID:        bson.NewObjectId(),
+		Title:     t.Title,
+		Completed: false,
+		CreatedAt: time.Now(),
+	}
+
+	if err := db.C(collectionName).Insert(&im); err != nil {
+		rnd.JSON(w, http.StatusProcessing, renderer.M{
+			"message": "Failed tp save item",
+			"error":   err,
+		})
+		return
+	}
+
+	rnd.JSON(w, http.StatusCreated, renderer.M{
+		"message": "Todo item created successfully",
+		"item_id": im.ID.Hex(),
+	})
+}
+
+func deleteItem(w http.ResponseWriter, r *http.Request) {
+	id := strings.TrimSpace(chi.URLParam(r, "id"))
+
+	if !bson.IsObjectIdHex(id) {
+		rnd.JSON(w, http.StatusBadRequest, renderer.M{
+			"message": "Invalid id",
+		})
+		return
+	}
+
+	if err := db.C(collectionName).RemoveId(bson.ObjectIdHex(id)); err != nil {
+		rnd.JSON(w, http.StatusProcessing, renderer.M{
+			"message": "Failed to delete item",
+			"error":   err,
+		})
+		return
+	}
+
+	rnd.JSON(w, http.StatusOK, renderer.M{
+		"message": "item deleted successfully",
+	})
+}
+
+func updateItem(w http.ResponseWriter, r *http.Request) {
+	id := strings.TrimSpace(chi.URLParam(r, "id"))
+
+	if !bson.IsObjectIdHex(id) {
+		rnd.JSON(w, http.StatusBadRequest, renderer.M{
+			"message": "Invalid id",
+		})
+		return
+	}
+
+	var t item
+
+	if err := json.NewDecoder(r.Body).Decode(&t); err != nil {
+		rnd.JSON(w, http.StatusProcessing, err)
+		return
+	}
+
+	if t.Title == "" {
+		rnd.JSON(w, http.StatusBadRequest, renderer.M{
+			"message": "Title field is required",
+		})
+		return
+	}
+
+	if err := db.C(collectionName).Update(
+		bson.M{"_id": bson.ObjectIdHex(id)},
+		bson.M{
+			"title":     t.Title,
+			"completed": t.Completed,
+		},
+	); err != nil {
+		rnd.JSON(w, http.StatusProcessing, renderer.M{
+			"message": "failed to update item",
+			"error":   err,
+		})
+		return
+	}
+
+	rnd.JSON(w, http.StatusOK, renderer.M{
+		"message": "Todo updated successfully",
+	})
+
 }
 
 func main() {
@@ -71,7 +197,7 @@ func main() {
 	}
 
 	go func() {
-		log.Println("Listening to port", port)
+		log.Println("Listening to port ", port)
 		if err := srv.ListenAndServe(); err != nil {
 			log.Printf("listen: %s\n", err)
 		}
@@ -79,7 +205,7 @@ func main() {
 
 }
 
-func todoHandlers() {
+func todoHandlers() http.Handler {
 	rg := chi.NewRouter()
 	rg.Group(func(r chi.Router) {
 		r.Get("/", fetchItems)
